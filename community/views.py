039f7d330018b6daf2a28d8f614eb9ccd26c2dcc
jn-97 <kjo5395@gmail.com>
2024-01-28 20:44:22 +0900

    feat: community-pagination

--- community/views.py
+++ community/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from .models import Post
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 def community(request):
+  page = request.GET.get('page', '1')
+  posts = Post.objects.all().order_by('-created_at')
+  paginator = Paginator(posts, 20)
+  page_obj = paginator.get_page(page)
 
-  posts = Post.objects.all()
+  context = {
+    'posts': page_obj
+  }
 
-  return render(request, 'community/community.html', {'posts': posts})
+  return render(request, 'community/community.html', context)
 
 @login_required
 def community_create(request):
